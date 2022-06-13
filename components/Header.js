29f42c9fb47c504114f0afdd4a9eff30d8029abd
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-06-13 20:56:53 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='text-green-500 text-2xl font-medium w-[90%] mx-auto py-3 tito'>
+    <div className='text-green-500 text-4xl font-medium w-[90%] mx-auto py-3 tito'>
         K's Spotify
     </div>
   )
